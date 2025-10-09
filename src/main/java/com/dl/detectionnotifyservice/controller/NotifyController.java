43d918499a9647e75dffaa267561c5a51ff1c333
Harin3Bone <harin.thananam@gmail.com>
2025-10-09 23:17:57 +0700

    Bug fix - Set properly HttpStatus response

--- src/main/java/com/dl/detectionnotifyservice/controller/NotifyController.java
+++ src/main/java/com/dl/detectionnotifyservice/controller/NotifyController.java
@@ -5,9 +5,11 @@ import com.dl.detectionnotifyservice.model.rest.NotifyResponse;
 import com.dl.detectionnotifyservice.service.NotifyService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
@@ -19,6 +21,7 @@ public class NotifyController {
     private final NotifyService notifyService;
 
     @PostMapping("/v1/send")
+    @ResponseStatus(HttpStatus.OK)
     public NotifyResponse sendNotify(@RequestBody NotifyRequest request) {
         log.info("Received request to send notification");
         return notifyService.publishNotifyPayload(request);
